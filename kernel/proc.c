c608ec828348b6429fc1feace22acc30b65610ef
Sofia Dostalkova <xdostalkovas@stuba.sk>
2023-11-08 15:16:09 +0100

    feat: zrychlenie sys volani

--- kernel/proc.c
+++ kernel/proc.c
@@ -119,6 +119,7 @@ allocproc(void)
       release(&p->lock);
     }
   }
+ 
   return 0;
 
 found:
@@ -131,6 +132,13 @@ found:
     release(&p->lock);
     return 0;
   }
+  //allocate a usyscall page
+  if((p->usyscall = (struct usyscall*)kalloc())==0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->usyscall->pid = p->pid;
 
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
@@ -139,7 +147,6 @@ found:
     release(&p->lock);
     return 0;
   }
-
   // Set up new context to start executing at forkret,
   // which returns to user space.
   memset(&p->context, 0, sizeof(p->context));
@@ -155,6 +162,8 @@ found:
 static void
 freeproc(struct proc *p)
 {
+  if(p->usyscall)
+    kfree((void*)p->usyscall);
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
@@ -197,11 +206,16 @@ proc_pagetable(struct proc *p)
   // trampoline.S.
   if(mappages(pagetable, TRAPFRAME, PGSIZE,
               (uint64)(p->trapframe), PTE_R | PTE_W) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 2, 0);
+    uvmfree(pagetable, 1);
+    return 0;
+  }
+
+  if(mappages(pagetable, USYSCALL, PGSIZE, (uint64)p->usyscall, PTE_R | PTE_U)<0){
     uvmunmap(pagetable, TRAMPOLINE, 1, 0);
     uvmfree(pagetable, 0);
     return 0;
   }
-
   return pagetable;
 }
 
@@ -210,6 +224,7 @@ proc_pagetable(struct proc *p)
 void
 proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
   uvmfree(pagetable, sz);
